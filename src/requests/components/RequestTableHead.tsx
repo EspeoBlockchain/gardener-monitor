563179ea0b5454372acc94ac85cfd58f67721a9b
Tomasz Agaciński <>
2019-07-10 14:58:47 +0200

    fixed table border

--- src/requests/components/RequestTableHead.tsx
+++ src/requests/components/RequestTableHead.tsx
@@ -3,5 +3,4 @@ import TableHead from '@material-ui/core/TableHead';
 
 
 export const RequestTableHead = styled(TableHead)`
-    border: 1px solid;
 ` as typeof TableHead;
--- src/requests/components/RequestTableTh.tsx
+++ src/requests/components/RequestTableTh.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
 
 export const RequestTableTh = styled.th`
-  border: 1px solid;
 `;
