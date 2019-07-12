2186da220759de1857bcd3d979f435a3ccafb42b
Tomasz Agaciński <>
2019-07-12 13:06:56 +0200

    changed statusIndicator animation to mixin

--- src/status/components/StatusIndicator.tsx
+++ src/status/components/StatusIndicator.tsx
@@ -1,7 +1,16 @@
-import styled, { DefaultTheme, ThemedStyledProps } from 'styled-components';
+import styled, { DefaultTheme, ThemedStyledProps, keyframes } from 'styled-components';
 import { State } from '../ServerStatus';
 
 export type Props = ThemedStyledProps<State, DefaultTheme>;
+
+const fadeInfadeOut =
+  keyframes`
+  {
+    0% { opacity: 0; }
+    50%   { opacity: 1; }
+    100% { opacity: 0; }
+  }
+`;
 
 export const StatusIndicator = styled.p<Props>`
   align-self: center;
@@ -9,11 +18,5 @@ export const StatusIndicator = styled.p<Props>`
   width: 25px;
   border-radius: 50%;
   background-color: ${({ status, theme }: Props) => status === 'alive' ? theme.colors.green : theme.colors.red};
-  animation: fadein 2s infinite;
-
-  @keyframes fadein {
-    0% { opacity: 0; }
-    50%   { opacity: 1; }
-    100% { opacity: 0; }
-  }
+  animation: ${fadeInfadeOut} 2s infinite;
 `;
