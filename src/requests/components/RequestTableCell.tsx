81644783f32269092e1a79518c25edc0411d397c
Tomasz Agaciński <>
2019-08-06 09:31:10 +0200

    removed unused chunk of code

--- src/requests/components/RequestTableCell.tsx
+++ src/requests/components/RequestTableCell.tsx
@@ -4,7 +4,6 @@ export const RequestTableCell = styled.td`
     box-sizing: border-box;
     text-align: center;
     padding: 10px;
-    /* width: 20%; */
     width: 100vw;
     word-wrap: break-word;
     font-size: ${props => props.theme.fontSizes.medium};
